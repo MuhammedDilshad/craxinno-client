c08e0ed7a4ab6422ea55e29122f401ea4e604e1a
dilshad <md.mdsar@gmail.com>
2024-02-01 22:56:47 +0530

    api update

--- src/Api/UserRegister.js
+++ src/Api/UserRegister.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4300" });
+// const API = axios.create({ baseURL: "http://localhost:4300" });
+const API = axios.create({ baseURL: "https://craxinno-backend.onrender.com" });
 
 export const signUp = (formData) => API.post("/auth/register", formData);
 
