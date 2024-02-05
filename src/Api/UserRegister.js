db87ac5dd5b0c6e96dbb28744b50f00761eede44
dilshad <md.mdsar@gmail.com>
2024-02-05 22:45:50 +0530

    api

--- src/Api/UserRegister.js
+++ src/Api/UserRegister.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4300" });
-// const API = axios.create({ baseURL: "https://craxinno-backend.onrender.com" });
+// const API = axios.create({ baseURL: "http://localhost:4300" });
+const API = axios.create({ baseURL: "https://craxinno-backend.onrender.com" });
 
 export const signUp = (formData) => API.post("/auth/register", formData);
 
 export const personalInfo = (formData) => API.post("/auth/personal", formData);
 
-// export const getUserData = (userId) => API.get(`/auth/${userId}`);
 export const getUserData = (userId) => API.get(`/auth/user/${userId}`);
